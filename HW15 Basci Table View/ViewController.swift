a048e502d45c4956a322ebfcadae7846d851cd77
Latifah <l123868@gmail.com>
2022-04-20 20:30:32 +0800

    Initial Commit

---
+++ HW15 Basci Table View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW15 Basci Table View
+//
+//  Created by Latifah on 2022/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW15 Basci Table ViewUITests/HW15_Basci_Table_ViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HW15_Basci_Table_ViewUITestsLaunchTests.swift
+//  HW15 Basci Table ViewUITests
+//
+//  Created by Latifah on 2022/4/20.
+//
+
+import XCTest
+
+class HW15_Basci_Table_ViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
